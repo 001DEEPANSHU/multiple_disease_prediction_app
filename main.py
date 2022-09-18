c5049d17968e576859b16a9326dcbc5c4c79fc3c
DEEPANSHU KAUSHIK <40486345+001DEEPANSHU@users.noreply.github.com>
2022-09-18 17:12:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diabetes_model = pickle.load(open('final_models/diab_model.sav', 'rb'))
-heart_disease_model = pickle.load(open('final_models/heart_model.sav','rb'))
-parkinsons_model = pickle.load(open('final_models/park_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diab_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_model.sav','rb'))
+parkinsons_model = pickle.load(open('park_model.sav', 'rb'))
 
 
 # Making a slidebar for navigation
